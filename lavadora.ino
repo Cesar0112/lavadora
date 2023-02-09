8b6d5c28fb49ebad8b4a44d94a90b8f52b137a81
Cesar0112 <hectorfernandezdiaz6@gmail.com>
2023-02-09 18:43:16 -0500

    No funciona como yo quisiera pero hace algo

--- lavadora.ino
+++ lavadora.ino
@@ -4,29 +4,31 @@
 #define TEN_MIN 10
 #define FIFTEEN_MIN 15
 
-int pin5min = 4;  // pin por el cual se va a leer la orden de 5 min
-int pin15min = 3; // pin por el cual se va a leer la orden de 15 min
+const int pin5min = 4;  // pin por el cual se va a leer la orden de 5 min
+const int pin15min = 3; // pin por el cual se va a leer la orden de 15 min
 int tiempoMin = 0;
-int pin4seg = 12;
-int pin8seg = 13;
-int pinBtn = 2;
-int pinDIN = 8;
-int pinCLK = 6;
-int pinLOAD = 7;
-int pinLed = 5;
+const int pin4seg = 13;
+const int pin8seg = 12;
+const int pinBtn = 7;
+const int pinDIN = 8;
+const int pinCLK = 6;
+const int pinLOAD = 9;
+const int pinLed = 5;
 unsigned long t4seg = 0;
 unsigned long t8seg = 0;
-int state4seg = LOW;
-int state8seg = LOW;
+int state4seg = HIGH;
+int state8seg = HIGH;
 int segundosTiempo = tiempoMin * 60;
-unsigned long tPrevReloj = 0;
-bool pausa = false;
+unsigned long tPrevReloj = millis();
+bool pausa = true;                                 // desde el principio esta en pausa
+int stateBtn = LOW;
 LedControl lc = LedControl(pinDIN, pinCLK, pinLOAD, 1); // pines del arduino DIN,CLK,LOAD,Cantidad de max7219
 void setup()
 {
   // put your setup code here, to run once:
   pinMode(pin15min, INPUT);
   pinMode(pin5min, INPUT);
+  pinMode(pinBtn, INPUT);
   pinMode(pin4seg, OUTPUT);
   pinMode(pin8seg, OUTPUT);
 
@@ -41,28 +43,45 @@ void setup()
 }
 
 void loop()
-{
+{ 
+  t4seg = millis();
+  t8seg = millis();
   // put your main code here, to run repeatedly:
   escojerTiempo();
-  
+
   /*Este codigo es para cambiar cada 4 u 8 segundos los estados de las salidas*/
   lavar();
   // se acaba el codigo para cambiar cada 4 u 8 segundos
-
-  if (digitalRead(pinBtn))
-  {
-    delay(100); // antirebote
-    pausa = !pausa;
+  
+  if (digitalRead(pinBtn) == HIGH)
+  {   delay(100);
+      bool solto = false;
+      while(!solto){
+             if(digitalRead(pinBtn) == LOW){
+                delay(100);
+                solto = true;
+             }
+      }
+      if(solto){
+        pausa=!pausa;
+        t4seg = millis();
+        t8seg = millis();
+        tPrevReloj = millis();
+      }
   }
   if (!pausa) // si no esta en pausa significa que esta lavando
   {
-    digitalWrite(pinLed, HIGH);
+    digitalWrite(pinLed, HIGH); // prende el led para visualizar el lavado
   }
   else
   {
     digitalWrite(pinLed, LOW);
   }
-  displayTime(); // muestra el tiempo
+  if(pausa){
+    digitalWrite(pin4seg, LOW);
+    digitalWrite(pin8seg, LOW);
+  }
+  /*displayTime(); // muestra el tiempo*/
 }
 
 // funcion que muestra el tiempo en decremento por el display de 7 segmentos
@@ -87,8 +106,8 @@ void displayTime()
 }
 void lavar()
 {
-  //este codigo es para lavar
-  if (!pausa && tiempoMin > 0)
+  // este codigo es para lavar
+  if (!pausa && segundosTiempo > 0)
   {
     if ((millis() - t4seg) >= 4000)
     {
@@ -102,27 +121,31 @@ void lavar()
       digitalWrite(pin8seg, !state8seg);
       state8seg = !state8seg; // cambia su estado
     }
+  }if(segundosTiempo<=0){
+    digitalWrite(pin8seg, LOW);
+    digitalWrite(pin4seg, LOW);
+    pausa = true;
   }
 }
-void escojerTiempo(){
-  if (digitalRead(pinBtn)) // si toca el boton para prender o despausar
-  {
-    delay(100);               // espera para evitar rebote
-    pausa = !pausa;           // despausar
-    if (digitalRead(pin5min)) // si se cumple significa que no puede cumplirse nadamas
+void escojerTiempo()
+{
+  if (pausa && tiempoMin==0){
+    if (digitalRead(pin5min) == HIGH) // si se cumple significa que no puede cumplirse nadamas
     {
+      delay(100);
       tiempoMin = FIVE_MIN;
       segundosTiempo = tiempoMin * 60; // convierte los minutos a segundos
     }
-    else if (digitalRead(pin15min))
-    {
+    else if (digitalRead(pin15min) == HIGH)
+    { 
+      delay(100);
       tiempoMin = FIFTEEN_MIN;
       segundosTiempo = tiempoMin * 60;
     }
-    else
-    { // si no se cumple ninguno de los anteriores significa que es 10 min
+    else{
       tiempoMin = TEN_MIN;
       segundosTiempo = tiempoMin * 60;
     }
   }
+  
 }
